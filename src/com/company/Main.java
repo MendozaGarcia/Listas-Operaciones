367ab4ed87206a48122303e940d2011dc0cc5eab
Uriel Mendoza <16urimendoza@gmail.com>
2017-11-01 14:24:14 -0600

    Listas Operaciones

---
+++ src/com/company/Main.java
@@ -0,0 +1,101 @@
+package com.company;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Lista lista = new Lista();
+
+        /*lista.agregarAlFinal(7);
+        lista.agregarAlFinal(8);
+        lista.agregarAlFinal(10);
+        lista.agregarAlFinal(16);*/
+
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean continuar = true;
+        int opcion =0;
+        do {
+            System.out.println(" ");
+            System.out.println("Elija una opción");
+            System.out.println("( 1 ) --> Insertar");
+            System.out.println("( 2 ) --> Mostrar");
+            System.out.println("( 3 ) --> Buscar");
+            System.out.println("( 4 ) --> Eliminar");
+            System.out.println("( 5 ) --> Tamaño");
+            System.out.println("( 6 ) --> Terminar");
+            System.out.println(" ");
+       try{
+            opcion = sc.nextInt();
+       } catch (Exception e) {
+           sc = new Scanner(System.in);// se inicializa nuevamente.
+           System.out.println("No es una opcion");
+           continue;
+       }
+
+            switch (opcion) {
+                case 1:
+
+                    int valor;
+                            System.out.println("<<--ingresa el número que deseas insertar-->> ");
+                    try {
+                            valor = sc.nextInt();
+                            lista.agregarAlFinal(valor);
+                        } catch (Exception e) {
+                         sc = new Scanner(System.in);// se inicializa nuevamente.
+                        System.out.println("No me diste un valor numerico");
+                        continue;
+                    }
+
+                            break;
+
+
+                case 2:
+                    System.out.println("<<--Lista simple -->>");
+                    lista.listar();
+                    System.out.println(" ");
+                    break;
+
+                case 3:
+                    if (lista.esVacia())
+                    {
+                        lista.listar();
+                        System.out.println(" ");
+
+                    }else {
+
+
+                        System.out.println(" valor a buscar");
+                        int num2=sc.nextInt();
+                        System.out.println(lista.buscar(num2));
+                        System.out.print(lista.getPosicion(num2));
+                    }
+
+
+                    break;
+
+                case 4:
+                    System.out.println("Elimina el nodo en la posición por indice");
+                    int num=sc.nextInt();
+                    lista.removerPorPosicion(num);
+                    lista.listar();
+                    System.out.println(" ");
+                    break;
+
+                case 5:
+                    System.out.println("Tamaño");
+                    System.out.println(lista.getTamaño());
+                    break;
+
+                case 6:
+                    System.out.println("Usted salio de la lista");
+
+                    break;
+            }
+        }while (opcion !=6);
+
+
+    }
+}
